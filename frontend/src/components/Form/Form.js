92485e99a9134624efdd991c97dbd9270d222bbe
gabriel1og <gogomide.go@gmail.com>
2023-01-31 13:58:28 -0300

    creating edit and submit logic to Form

--- frontend/src/components/Form/Form.js
+++ frontend/src/components/Form/Form.js
@@ -1,9 +1,69 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import * as F from "./styles"
+import axios from "axios";
+import { toast } from "react-toastify";
 
-export default function Form() {
+export default function Form({ getUsers, onEdit, setOnEdit }) {
+    const ref = useRef();
+
+    useEffect(() => {
+        if (onEdit) {
+            const user = ref.current;
+
+            user.nome.value = onEdit.nome;
+            user.email.value = onEdit.email;
+            user.fone.value = onEdit.fone;
+            user.data_nascimento.value = onEdit.data_nascimento;
+        }
+    }, [onEdit]);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const user = ref.current;
+
+        if (
+            !user.nome.value ||
+            !user.email.value ||
+            !user.fone.value ||
+            !user.data_nascimento.value
+        ) {
+            return toast.warn("Preencha todos os campos!");
+        }
+
+        if (onEdit) {
+            await axios
+                .put("http://localhost:8800/" + onEdit.id, {
+                    nome: user.nome.value,
+                    email: user.email.value,
+                    fone: user.fone.value,
+                    data_nascimento: user.data_nascimento.value,
+                })
+                .then(({ data }) => toast.success(data))
+                .catch(({ data }) => toast.error(data));
+        } else {
+            await axios
+                .post("http://localhost:8800", {
+                    nome: user.nome.value,
+                    email: user.email.value,
+                    fone: user.fone.value,
+                    data_nascimento: user.data_nascimento.value,
+                })
+                .then(({ data }) => toast.success(data))
+                .catch(({ data }) => toast.error(data));
+        }
+
+        user.nome.value = "";
+        user.email.value = "";
+        user.fone.value = "";
+        user.data_nascimento.value = "";
+
+        setOnEdit(null);
+        getUsers();
+    };
+
     return (
-        <F.FormContainer>
+        <F.FormContainer ref={ref} onSubmit={handleSubmit}>
             <F.InputArea>
                 <F.Label>Nome</F.Label>
                 <F.Input name="nome" />
