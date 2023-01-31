857ebd11c304e70e3b6e42872ae0f49856d6ed03
gabriel1og <gogomide.go@gmail.com>
2023-01-31 12:13:36 -0300

    creating crud functions

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -9,3 +9,49 @@ export const getUsers = (_, res) => {
         return res.status(200).json(data);
     })
 }
+
+export const addUser = (req, res) => {
+    const qry =
+        "INSERT INTO usuarios(`nome`, `email`, `fone`, `data_nascimento`) VALUES(?)";
+
+    const values = [
+        req.body.nome,
+        req.body.email,
+        req.body.fone,
+        req.body.data_nascimento,
+    ];
+
+    db.query(qry, [values], (err) => {
+        if (err) return res.json(err);
+
+        return res.status(200).json("Usuário criado com sucesso.");
+    });
+};
+
+export const updateUser = (req, res) => {
+    const qry =
+        "UPDATE usuarios SET `nome` = ?, `email` = ?, `fone` = ?, `data_nascimento` = ? WHERE `id` = ?";
+
+    const values = [
+        req.body.nome,
+        req.body.email,
+        req.body.fone,
+        req.body.data_nascimento,
+    ];
+
+    db.query(qry, [...values, req.params.id], (err) => {
+        if (err) return res.json(err);
+
+        return res.status(200).json("Usuário atualizado com sucesso.");
+    });
+};
+
+export const deleteUser = (req, res) => {
+    const qry = "DELETE FROM usuarios WHERE `id` = ?";
+
+    db.query(qry, [req.params.id], (err) => {
+        if (err) return res.json(err);
+
+        return res.status(200).json("Usuário deletado com sucesso.");
+    });
+};
